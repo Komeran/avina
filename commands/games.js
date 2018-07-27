c852e550838257fa76d68552bfba53c15a0521eb
Marc Schäfer <marc.schaefer@rabcat.local>
2018-07-27 11:25:51 +0200

    Refined !games command list to an embedded message

--- commands/games.js
+++ commands/games.js
@@ -2,15 +2,23 @@ var logger = require('winston');
 var games = require('../dnd_util/games.js');
 
 module.exports = function(args, message) {
-    let msg = "List of currently running games:"
+    let embed = {
+        color: 3447003,
+        title: "List of running Games",
+        description: "",
+        fields: []
+    };
+
     games.forEach(function(game) {
-        msg += "\n" + game.session + "(DM: <@" + game.dm + ">, players: " + game.players.length + ")";
+        embed.fields.push({
+            name: game.session,
+            value: "DM: <@" + game.dm + ">\nPlayers: " + game.players.length
+        });
     }, this);
 
     if(games.length === 0) {
-        message.channel.send("There are no games currently! Be the first to start one using !claimdm <Game> , or have your DM do it!");
-        return;
+        embed.description = "There are no games currently! Be the first to start one using !claimdm <Game> , or have your DM do it!";
     }
 
-    message.channel.send(msg);
+    message.channel.send(embed);
 };
