bc94abfe0e9814f166e3e27ab1076d86d0da0814
Marc Schäfer <sch15916@spengergasse.at>
2018-07-27 21:22:02 +0200

    Added disapprove command

---
+++ commands/disapprove.js
@@ -0,0 +1,24 @@
+var logger = require('winston');
+var applications = require('../util/applications.js');
+
+module.exports = function(args, message) {
+    if(!message.guild.member(message.author).hasPermission('ADMINISTRATOR')) {
+        message.author.send("Only admins of a server may use the !disapprove command! And you are no admin, sorry :/");
+        return;
+    }
+
+    if(args.length >= 2) {
+        message.mentions.members.every(function(user) {
+            for(let a in applications) {
+                if(applications[a].user === user.id) {
+                    user.send(message.author.username + " has disapproved your role applications!");
+                    applications.splice(Number(a), 1);
+                    return;
+                }
+            }
+        });
+    }
+    else if(args.length === 1) {
+        message.author.send('\'disapprove\' command invalid: Missing user parameter!');
+    }
+};
