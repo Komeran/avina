ba3da6c65e1b723afa5582eb0cfba7aae53e7a69
Komeran <komerankesum@gmail.com>
2018-10-30 11:46:14 +0100

    Fixed pso2 help

--- commands/pso2.js
+++ commands/pso2.js
@@ -8,6 +8,17 @@ let logger = require('winston');
  */
 class pso2 extends ParentCommand {
 
+    constructor() {
+        super();
+        this.help = function(args, message) {
+            if(args[2] !== null && args[2] !== undefined && this.subCommands[args[2]] !== undefined && this.subCommands[args[2]] !== null) {
+                return this.subCommands[args[2]].help;
+            } else {
+                return "`!pso2 [itemname|shop] (<arguments>)` in order to execute the corresponding command for the game pso2.\n"+
+                    "use `!help pso2 [itemname|shop] (<arguments>)` in order to receive help for the corresponding command.";
+            }
+        };
+    }
     /**
      *
      * @param inp {string} Input string name of the item either in English or Japanese
@@ -37,13 +48,5 @@ class pso2 extends ParentCommand {
 }
 
 pso2.subCommandDirectory = "custom_command_logic/pso2";
-pso2.help = function(args, message) {
-    if(args[2] !== null && args[2] !== undefined && this.subCommands[args[2]] !== undefined && this.subCommands[args[2]] !== null) {
-        return this.subCommands[args[2]].help;
-    } else {
-        return "`!pso2 [itemname|shop] (<arguments>)` in order to execute the corresponding command for the game pso2.\n"+
-            "use `!help pso2 [itemname|shop] (<arguments>)` in order to receive help for the corresponding command.";
-    }
-};
 
 module.exports = pso2;
