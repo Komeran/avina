02eb8154784e9b94d85a02b75983f4f45473664c
Komeran <komerankesum@gmail.com>
2018-10-30 17:52:22 +0100

    Migrated clear command

--- commands/clear.js
+++ commands/clear.js
@@ -3,14 +3,32 @@
  * @date 24.08.2018
  */
 
-module.exports = {
-    execute: function (args, message) {
+let logger = require('winston');
+const BaseCommand = require("../util/BaseCommand");
+const Message = require("discord.js").Message;
+
+class Clear extends BaseCommand {
+    constructor() {
+        super();
+        this.help = "";
+    }
+
+    /**
+     * @override
+     * @param args {string[]}
+     * @param message {Message}
+     */
+    execute(args, message) {
         if (message.member.hasPermission("MANAGE_MESSAGES")) {
             message.channel.fetchMessages()
                 .then(function(list){
                     message.channel.bulkDelete(list);
-                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})
+                }, function(err){
+                    message.channel.send("ERROR: ERROR CLEARING CHANNEL.");
+                    logger.error(err);
+                })
         }
-    },
-    help: ""
-};
+    }
+}
+
+module.exports = Clear;
