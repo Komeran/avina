ceeced67b177319f298ec9c10d63f5dc9ce06306
Komeran <komerankesum@gmail.com>
2018-10-30 17:33:55 +0100

    debug pso2 command

--- commands/pso2.js
+++ commands/pso2.js
@@ -37,6 +37,7 @@ class pso2 extends ParentCommand {
      */
     static returnJson(itemname) {
         return new Promise(function(resolve) {
+            console.log("itemname", itemname);
             request('http://db.kakia.org/item/search?name='+encodeURIComponent(itemname), function(e,r,dom) {
                 if(!e && r.statusCode === 200) {
                     console.log(dom);
