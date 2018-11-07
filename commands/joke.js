7dd2338251bdb5b5bc18fc86544bfb8bc177cdfb
Komeran <komerankesum@gmail.com>
2018-11-07 09:43:01 +0100

    Added joke command

---
+++ commands/joke.js
@@ -0,0 +1,64 @@
+/**
+ * @author marc.schaefer
+ * @date 07.11.2018
+ */
+
+let logger = require('winston');
+const BaseCommand = require("../util/BaseCommand");
+const Message = require("discord.js").Message;
+const request = require("request");
+
+class joke extends BaseCommand {
+    constructor() {
+        super();
+        this.help = "";
+    }
+
+    /**
+     *
+     * @param args {string[]}
+     * @param message {Message}
+     */
+    execute(args, message) {
+        let searchTerm = null;
+        if(args.length > 1) {
+            searchTerm = args[1];
+            for(let i = 2; i < args.length; i++) {
+                searchTerm += " " + args[i];
+            }
+        }
+
+        let wasCommand = args[0] !== null;
+
+        if(searchTerm) {
+            request({
+                method: 'GET',
+                uri: "https://icanhazdadjoke.com/search?term=" + searchTerm,
+                headers: {
+                    'Accept': 'text/plain'
+                }
+            }, function(error, response, body) {
+                let jokes = body.split('\n');
+                let randomJoke = Math.floor(Math.random() * jokes.length);
+                message.channel.send(jokes[randomJoke]);
+                if(wasCommand)
+                    message.delete();
+            });
+        }
+        else {
+            request({
+                method: 'GET',
+                uri: "https://icanhazdadjoke.com/",
+                headers: {
+                    'Accept': 'text/plain'
+                }
+            }, function(error, response, body) {
+                message.channel.send(body);
+                if(wasCommand)
+                    message.delete();
+            });
+        }
+    }
+}
+
+module.exports = joke;
