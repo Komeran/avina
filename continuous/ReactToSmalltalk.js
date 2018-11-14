fb842d836cc1cef574da95edabba663318ee4deb
Komeran <komerankesum@gmail.com>
2018-11-14 14:36:17 +0100

    Fixed ReactToSmalltalk

--- continuous/ReactToSmalltalk.js
+++ continuous/ReactToSmalltalk.js
@@ -7,6 +7,7 @@ let logger = require('winston');
 const MessageListener = require("../util/continuous/base_listeners/MessageListener.js");
 const Message = require("discord.js").Message;
 let reactTo = require('../util/reactTo.js');
+let dbClient = require('../databaseClient.js');
 
 class ReactToSmalltalk extends MessageListener {
     /**
@@ -14,18 +15,22 @@ class ReactToSmalltalk extends MessageListener {
      * @param message {Message}
      */
     execute(message) {
-        if(!message.guild && message.author.id !== message.client.user.id) {
-            reactTo(message, message.client.user.id);
-            return;
-        }
-        if(message.author.id === message.client.user.id || isIgnored)
-            return;
-        for(let user of message.mentions.users) {
-            if(user[0] === message.client.user.id) {
+        dbClient.getTextChannel(message.channel.id).then(function(textChannel) {
+            let isIgnored = !!textChannel && !!textChannel.ignoreCommands;
+
+            if(!message.guild && message.author.id !== message.client.user.id) {
                 reactTo(message, message.client.user.id);
                 return;
             }
-        }
+            if(message.author.id === message.client.user.id || isIgnored)
+                return;
+            for(let user of message.mentions.users) {
+                if(user[0] === message.client.user.id) {
+                    reactTo(message, message.client.user.id);
+                    return;
+                }
+            }
+        }).catch(logger.error);
     }
 }
 
